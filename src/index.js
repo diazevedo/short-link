758440fd3f9df3e1d4b2dbc748ef73c94b99019a
Diego Azevedo <dapptn@hotmail.com>
2020-02-20 05:49:40 +1100

    Hits

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "./styles/scss/reset.scss";
 import "./styles/scss/main.scss";
 import "./styles/scss/header.scss";
 import "./styles/scss/section-short-link.scss";
+import "./styles/scss/top-hits.scss";
