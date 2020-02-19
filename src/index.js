d0f9711a1d76b6ab8f44b9c115be6ee898835d10
Diego Azevedo <dapptn@hotmail.com>
2020-02-20 07:02:38 +1100

    working progress

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import "./styles/scss/main.scss";
 import "./styles/scss/header.scss";
 import "./styles/scss/section-short-link.scss";
 import "./styles/scss/top-hits.scss";
+import "./styles/scss/total-hits.scss";
+import "./styles/scss/footer.scss";
