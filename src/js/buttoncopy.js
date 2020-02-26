ab4d1a694edb2182a0072591a328547105ac5eb9
Diego Azevedo <dapptn@hotmail.com>
2020-02-27 02:22:01 +1100

    Buttons done

--- src/js/buttoncopy.js
+++ src/js/buttoncopy.js
@@ -1,14 +1,14 @@
 (function() {
-  const $copyButton = document.querySelector("#button-copy");
-  const $button = document.querySelector("#button-shortener");
-  const $inputUrl = document.querySelector("[name=url]");
+  console.log("ddd");
+  // const $copyButton = document.querySelector("#button-copy");
+  // const $button = document.querySelector("#button-shortener");
+  // const $inputUrl = document.querySelector("[name=url]");
 
-  $copyButton.addEventListener("click", () => {
-    $inputUrl.select();
-    $inputUrl.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-
-    $copyButton.classList.toggle("hidden");
-    $button.classList.toggle("hidden");
-  });
+  // $copyButton.addEventListener("click", () => {
+  // $inputUrl.select();
+  // $inputUrl.setSelectionRange(0, 99999);
+  // document.execCommand("copy");
+  // $copyButton.classList.toggle("hidden");
+  // $button.classList.toggle("hidden");
+  // });
 })();
--- src/js/shortener.js
+++ src/js/shortener.js
@@ -3,9 +3,11 @@
 
   const $form = document.querySelector("form");
   const $input = document.querySelector("input");
-  const $copyButton = document.querySelector("#button-copy");
 
-  const $button = document.querySelector("#button-shortener");
+  const $spanShortener = document.querySelector("#span-shortener");
+  const $spanCopy = document.querySelector("#span-copy");
+
+  const $button = document.querySelector("#button");
 
   const xhr = new XMLHttpRequest();
 
@@ -13,20 +15,52 @@
     if (xhr.status === 200) {
       const res = JSON.parse(xhr.response);
       $input.value = res.data.shortUrl.secure;
-      $button.classList.add("hidden");
-      $copyButton.classList.remove("hidden");
+      toggleButtons();
     }
   };
+
+  function toggleButtons() {
+    if ($button.classList.contains("shortener")) {
+      $button.classList.add("copy");
+      $button.classList.remove("shortener");
+
+      $spanShortener.classList.add("fade-out");
+      $spanShortener.classList.remove("fade-in");
+
+      $spanCopy.classList.add("fade-in");
+      $spanCopy.classList.remove("fade-out");
+
+      $spanShortener.classList.add("hidden");
+
+      $input.classList.toggle("searched");
+    } else if ($button.classList.contains("copy")) {
+      $button.classList.remove("copy");
+      $button.classList.add("shortener");
+
+      $spanShortener.classList.add("fade-in");
+      $spanShortener.classList.remove("fade-out");
+
+      $spanCopy.classList.add("fade-out");
+      $spanCopy.classList.remove("fade-in");
+
+      $input.classList.toggle("searched");
+    }
+  }
 
   $form.addEventListener("submit", event => {
     event.preventDefault();
 
-    xhr.open(
-      "GET",
-      `https://cu8.in/api/?action=short&urls=|https://${$input.value}`,
-      true
-    );
+    if ($button.classList.contains("shortener")) {
+      console.log(1111);
+      xhr.open(
+        "GET",
+        `https://cu8.in/api/?action=short&urls=|https://${$input.value}`,
+        true
+      );
 
-    xhr.send();
+      xhr.send();
+    } else {
+      toggleButtons();
+    }
   });
 })();
